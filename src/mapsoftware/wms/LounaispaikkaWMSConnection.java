fffb0ec76b1fff2968a60ce7a4b7d661e4f87a78
Ville Ahti <vianah@utu.fi>
2013-01-24 11:04:20 +0200

    WMSConnectionStrategy-rajapinta kommentoitu

--- src/mapsoftware/wms/LounaispaikkaWMSConnection.java
+++ src/mapsoftware/wms/LounaispaikkaWMSConnection.java
@@ -1,9 +1,7 @@
 package mapsoftware.wms;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
@@ -13,19 +11,17 @@ import java.net.UnknownHostException;
 import java.util.List;
 
 public class LounaispaikkaWMSConnection implements WMSConnectionStrategy {
-	
+
 	private static final String GET_MAP_STATIC_PART = "http://kartat.lounaispaikka.fi/wms/maakuntakaava?version=1.1.1&service=WMS&request=getmap&srs=EPSG:4326&&width=700&height=500&format=image/png&styles=&";
 	private static final String GET_CAP_REQ_ADDRESS = "/wms/maakuntakaava?version=1.1.1&service=WMS&request=GetCapabilities";
 	private static final int HTTP_PORT = 80;
 	private static final String HOST = "kartat.lounaispaikka.fi";
 	private Socket s;
 	private final WMSCapabilitiesParser parser;
-	
-	
+
 	public LounaispaikkaWMSConnection(WMSCapabilitiesParser parser) {
 		this.parser = parser;
 	}
-	
 
 	@Override
 	public List<LayerInformation> getCapabilities() {
@@ -36,22 +32,22 @@ public class LounaispaikkaWMSConnection implements WMSConnectionStrategy {
 			pw = new PrintWriter(s.getOutputStream());
 			is = s.getInputStream();
 		} catch (UnknownHostException e) {
-			//problem with constant address
+			// problem with constant address
 			e.printStackTrace();
 		} catch (IOException e) {
-			//socket problem
+			// socket problem
 			e.printStackTrace();
 		}
-		
+
 		pw.println("GET " + GET_CAP_REQ_ADDRESS + " HTTP/1.1");
 		pw.println("Host: " + HOST);
 		pw.println("");
 		pw.flush();
-		
+
 		List<LayerInformation> result = parser.parseDocument(is);
 		pw.close();
 		try {
-			is.close(); 
+			is.close();
 			s.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -63,14 +59,13 @@ public class LounaispaikkaWMSConnection implements WMSConnectionStrategy {
 	@Override
 	public URL getMap(String layers, String area) {
 		try {
-			//dynamic URL generation here
-			return new URL(GET_MAP_STATIC_PART + "layers=" + layers + "&" + "bbox=" + area);
-			
-			
+			// dynamic URL generation here
+			return new URL(GET_MAP_STATIC_PART + "layers=" + layers + "&"
+					+ "bbox=" + area);
+
 		} catch (MalformedURLException e) {
 			return null;
 		}
 	}
-	
 
 }
--- src/mapsoftware/wms/WMSConnectionStrategy.java
+++ src/mapsoftware/wms/WMSConnectionStrategy.java
@@ -3,7 +3,31 @@ package mapsoftware.wms;
 import java.net.URL;
 import java.util.List;
 
+
+/**
+ * <p>WMS protocol interface for doing basic WMS requests</p>
+ * 
+ * @author Ville Ahti
+ * @author Aleksi Haapsaari
+ * @author Johannes Miettinen
+ *
+ */
 public interface WMSConnectionStrategy {
-	 List<LayerInformation> getCapabilities();
-	 URL getMap(String layers, String area);
+	
+	 /**
+	  * <p>Protocol method for getting layers and other information</p>
+	  * 
+	 * @return list of services map layers. 
+	 */
+	List<LayerInformation> getCapabilities();
+	 
+	
+	 /**
+	  * <p>Returns a URL to requested map.</p>
+	  * 
+	 * @param layers gotten from getCapabilities
+	 * @param area location parameters
+	 * @return URL which has the requested map 
+	 */
+	URL getMap(String layers, String area);
 }
