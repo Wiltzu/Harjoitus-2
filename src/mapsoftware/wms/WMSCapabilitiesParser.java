b08c869024d95669e58c8c7bea0c1b8e8b8b1c5c
Ville Ahti <vianah@utu.fi>
2013-02-07 12:33:21 +0200

    ideas to implement

--- src/mapsoftware/wms/WMSCapabilitiesParser.java
+++ src/mapsoftware/wms/WMSCapabilitiesParser.java
@@ -6,12 +6,18 @@ import java.util.List;
 import mapsoftware.wms.domain.LayerInformation;
 
 public interface WMSCapabilitiesParser {
-	/**
-	 * <p>Returns a List containing LayerInformation fetched from the InputStream.</p>
-	 * 
-	 * @param xmlDocument InputStream containing getCapabilities request xml
-	 * @return List containing LayerInformation.
-	 *  
-	 */
-	List<LayerInformation> parseDocument(InputStream xmlDocument);
+    // TODO: parseDocument parses only document and return nothing, getLayers
+    // gives LayerInformation and getLocationBoundaries gives
+    // LocationInformation
+    /**
+     * <p>
+     * Returns a List containing LayerInformation fetched from the InputStream.
+     * </p>
+     * 
+     * @param xmlDocument
+     *            InputStream containing getCapabilities request xml
+     * @return List containing LayerInformation.
+     * 
+     */
+    List<LayerInformation> parseDocument(InputStream xmlDocument);
 }
