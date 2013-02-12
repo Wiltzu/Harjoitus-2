456d54c6118c70c7d2c2eb44df3843727010b3ff
Wiltzu <ville.ahti2@gmail.com>
2013-02-12 11:29:32 +0200

    LocationInformation refactored, still some little issues

--- src/mapsoftware/wms/domain/LocationInformation.java
+++ src/mapsoftware/wms/domain/LocationInformation.java
@@ -7,24 +7,11 @@ import java.util.Arrays;
  * Container for the location information
  * </p>
  * 
- * @author Johannes Miettinen
- * @author Aleksi Haapsaari
  * @author Ville Ahti
  * 
  */
 public class LocationInformation {
     // TODO: Comments
-    private final double[] maxCoords;
-    private final double[] currentCoords;
-    private final double angle;
-    private static final int MIN_X = 0, MIN_Y = 1, MAX_X = 2, MAX_Y = 3;
-    private double latDistance, longDistance, latMovement, longMovement;
-    // TODO: do with enums
-    private static double[] zoomLevels = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
-            0.8, 0.9, 1 };
-    private double[] zoomLatDistances, zoomLongDistances;
-    private int zoomLevel;
-
     public enum Operation {
         LEFT, RIGHT, UP, DOWN, ZOOM_IN, ZOOM_OUT;
     }
@@ -119,7 +106,7 @@ public class LocationInformation {
         return coordinants.substring(0, coordinants.length() - 1);
     }
 
-    // TODO: String type to enum
+    // TODO: mayby 20 level zoom
     /**
      * <p>
      * Changes the coordinates according to which parameter is given.
@@ -130,6 +117,7 @@ public class LocationInformation {
      *            changed.
      */
     public void move(Operation operation) {
+    	int lastZoomLevel = zoomLevel;
         updateMovement();
         switch (operation) {
         case LEFT:
@@ -149,75 +137,68 @@ public class LocationInformation {
             currentCoords[MAX_Y] -= latMovement;
             break;
         case ZOOM_IN:
-            // TODO: reduce the gap between min and max not their location!
-            zoomLevel--;
-            // difference between current and becoming distance between points
-            // min(x,y) and max(x,y)
-            double currentLatZoom = zoomLatDistances[zoomLevel + 1]
-                    - zoomLatDistances[zoomLevel];
-            double currentLongZoom = zoomLongDistances[zoomLevel + 1]
-                    - zoomLongDistances[zoomLevel];
-            // calculating new points (moves both points half the wanted
-            // distance closer to each other)
-            currentCoords[MAX_X] = currentCoords[MIN_X]
-                    + (zoomLongDistances[zoomLevel] - (currentLongZoom / 2));
-            currentCoords[MAX_Y] = currentCoords[MIN_Y]
-                    + (zoomLatDistances[zoomLevel] - (currentLatZoom / 2));
-
-            currentCoords[MIN_X] += currentLongZoom / 2;
-            currentCoords[MIN_Y] += currentLatZoom / 2;
+        	zoomLevel--;
+            doZoom(lastZoomLevel);
             break;
         case ZOOM_OUT:
             zoomLevel++;
-            // difference between current and becoming distance between points
-            // min(x,y) and max(x,y)
-            double currentLatZoom1 = zoomLatDistances[zoomLevel - 1]
-                    - zoomLatDistances[zoomLevel];
-            double currentLongZoom1 = zoomLongDistances[zoomLevel - 1]
-                    - zoomLongDistances[zoomLevel];
-            // calculating new points (moves both points half the wanted
-            // distance closer to each other)
-            currentCoords[MAX_X] = currentCoords[MIN_X]
-                    + (zoomLongDistances[zoomLevel] - (currentLongZoom1 / 2));
-            currentCoords[MAX_Y] = currentCoords[MIN_Y]
-                    + (zoomLatDistances[zoomLevel] - (currentLatZoom1 / 2));
-
-            currentCoords[MIN_X] += currentLongZoom1 / 2;
-            currentCoords[MIN_Y] += currentLatZoom1 / 2;
-            // fixes coordinates if over max
-            double[] coordsFixes = new double[4];
-            if (currentCoords[MIN_X] < maxCoords[MIN_X]) {
-                coordsFixes[MIN_X] = maxCoords[MIN_X] - currentCoords[MIN_X];
-                currentCoords[MIN_X] = maxCoords[MIN_X];
-            }
-            if (currentCoords[MIN_Y] < maxCoords[MIN_Y]) {
-                coordsFixes[MIN_Y] = maxCoords[MIN_Y] - currentCoords[MIN_Y];
-                currentCoords[MIN_Y] = maxCoords[MIN_Y];
-            }
-
-            if (currentCoords[MAX_X] > maxCoords[MAX_X]) {
-                coordsFixes[MAX_X] = maxCoords[MAX_X] - currentCoords[MAX_X];
-                currentCoords[MAX_X] = maxCoords[MAX_X];
-            }
-            if (currentCoords[MIN_X] > maxCoords[MAX_X]) {
-                coordsFixes[MAX_Y] = maxCoords[MAX_Y] - currentCoords[MAX_Y];
-                currentCoords[MAX_Y] = maxCoords[MAX_Y];
-            }
-
-            currentCoords[MIN_X] += coordsFixes[MAX_X];
-            currentCoords[MIN_Y] += coordsFixes[MAX_Y];
-            currentCoords[MAX_X] += coordsFixes[MIN_X];
-            currentCoords[MAX_Y] += coordsFixes[MIN_Y];
+            doZoom(lastZoomLevel);
+            fixCoordinates();
             break;
         }
     }
-
+    
     @Override
     public String toString() {
         return "LocationInformation [maxCoords=" + Arrays.toString(maxCoords)
                 + ", currentCoords=" + Arrays.toString(currentCoords)
                 + ", angle=" + angle + "]";
     }
+
+	private void fixCoordinates() {
+		// fixes coordinates if over max
+		double[] coordsFixes = new double[4];
+		if (currentCoords[MIN_X] < maxCoords[MIN_X]) {
+		    coordsFixes[MIN_X] = maxCoords[MIN_X] - currentCoords[MIN_X];
+		    currentCoords[MIN_X] = maxCoords[MIN_X];
+		}
+		if (currentCoords[MIN_Y] < maxCoords[MIN_Y]) {
+		    coordsFixes[MIN_Y] = maxCoords[MIN_Y] - currentCoords[MIN_Y];
+		    currentCoords[MIN_Y] = maxCoords[MIN_Y];
+		}
+
+		if (currentCoords[MAX_X] > maxCoords[MAX_X]) {
+		    coordsFixes[MAX_X] = maxCoords[MAX_X] - currentCoords[MAX_X];
+		    currentCoords[MAX_X] = maxCoords[MAX_X];
+		}
+		if (currentCoords[MIN_X] > maxCoords[MAX_X]) {
+		    coordsFixes[MAX_Y] = maxCoords[MAX_Y] - currentCoords[MAX_Y];
+		    currentCoords[MAX_Y] = maxCoords[MAX_Y];
+		}
+
+		currentCoords[MIN_X] += coordsFixes[MAX_X];
+		currentCoords[MIN_Y] += coordsFixes[MAX_Y];
+		currentCoords[MAX_X] += coordsFixes[MIN_X];
+		currentCoords[MAX_Y] += coordsFixes[MIN_Y];
+	}
+
+	private void doZoom(int lastZoomLevel) {
+		// difference between current and becoming distance between points
+		// min(x,y) and max(x,y)
+		double currentLatZoom = zoomLatDistances[lastZoomLevel]
+		        - zoomLatDistances[zoomLevel];
+		double currentLongZoom = zoomLongDistances[lastZoomLevel]
+		        - zoomLongDistances[zoomLevel];
+		// calculating new points (moves both points half the wanted
+		// distance closer to each other)
+		currentCoords[MAX_X] = currentCoords[MIN_X]
+		        + (zoomLongDistances[zoomLevel] - (currentLongZoom / 2));
+		currentCoords[MAX_Y] = currentCoords[MIN_Y]
+		        + (zoomLatDistances[zoomLevel] - (currentLatZoom / 2));
+
+		currentCoords[MIN_X] += currentLongZoom / 2;
+		currentCoords[MIN_Y] += currentLatZoom / 2;
+	}
 
     private void countDistances() {
         longDistance = (maxCoords[MAX_X] - maxCoords[MIN_X]);
@@ -239,4 +220,16 @@ public class LocationInformation {
             longMovement = 0;
         }
     }
+    
+    private final double[] maxCoords;
+    private final double[] currentCoords;
+    private final double angle;
+    private static final int MIN_X = 0, MIN_Y = 1, MAX_X = 2, MAX_Y = 3;
+    private double latDistance, longDistance, latMovement, longMovement;
+    // TODO: do with enums
+    private static double[] zoomLevels = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
+            0.8, 0.9, 1 };
+    private double[] zoomLatDistances, zoomLongDistances;
+    private int zoomLevel;
+    
 }
